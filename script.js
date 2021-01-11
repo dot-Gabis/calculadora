0394fe7cda6c90dccc3a748db02a819e832db3e4
Gabrielle <61875991+dot-Gabis@users.noreply.github.com>
2021-01-11 01:10:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function soma(){
+    let num1 = colocaralgumacoisa;
+    let num2 = colocaralgumacoisa2;
+}
+
+function subtracao() {
+
+}
+
+function mutiplicacao() {
+
+}
+
+function divisao() {
+    
+}
